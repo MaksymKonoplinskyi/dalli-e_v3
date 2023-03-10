7426bd0b0e088fab4d6708e06c0c7a99e29f9c57
MaksymKonoplinskyi <gran7777777@gmail.com>
2023-03-10 12:43:39 +0200

    r

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       screens: {
@@ -17,4 +17,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}; 
